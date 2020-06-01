47e37961d21b39ce9710ac77a761e86de6ebaa64
apple <apple@appleui-MacBookPro.local>
2020-06-02 00:39:55 +0900

    data preprcessor func

---
+++ src/data_prep/data_preprocessing.py
@@ -0,0 +1,28 @@
+import re
+import pandas as pd
+import multiprocessing as mp
+import numpy as np
+
+
+def remove():
+
+
+class Preprocessor:
+    def __init__(self, ):
+        self. 
+
+    def _get():
+
+        return 
+
+
+
+if __name__ == "__main__":
+    import time
+    start_time = time.time()
+
+    NAVER_PLACE_DATA_PATH = os.path.join("..","..","data","JS_05_reviews.pkl")
+    df = pd.read_pickle(NAVER_PLACE_DATA_PATH)
+
+    print(df.head())
+    print('elapsed time : {}'.format(time.time() - start_time))
